47cd6b7977bc8d2ce19c9d1a90cd9920af92dad4
Felipe300 <caracoloro@gmail.com>
2022-06-08 21:41:22 -0400

    feat: Add Rating

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'Orange': '#fb7413', // hsl(25, 97%, 53%)
+				'Light-Grey': '#959eac', // hsl(217, 12%, 63%)
+				'Medium-Grey': '#7c8798', // hsl(216, 12%, 54%)
+				'Medium-Grey-New': '#7c879833', // hsl(216, 12%, 54%)
+				'Dark-Blue': '#252d37', // hsl(213, 19%, 18%)
+				'Very-Dark-Blue': '#121417' // hsl(216, 12%, 8%)
+			}
+		},
+	},
+	plugins: [],
+}
